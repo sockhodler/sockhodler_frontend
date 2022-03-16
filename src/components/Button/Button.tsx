2a873822dc3c76e4de9fb0eff3e0f9b57475234b
talentjo <igrambilchester@gmail.com>
2022-03-16 18:27:14 +0330

    Add upload media info

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import classes from "./Button.module.scss";
 import ReactTooltip from "react-tooltip";
+import { ReactComponent as LoadingIcon } from "assets/icons/loading-spinner.svg";
 
 interface Props {
   className?: string;
@@ -12,6 +13,7 @@ interface Props {
   type?: "button" | "submit";
   disabled?: boolean;
   tooltip?: string;
+  loading?: boolean;
 }
 
 export const Button: React.FunctionComponent<Props> = ({
@@ -24,6 +26,7 @@ export const Button: React.FunctionComponent<Props> = ({
   type = "button",
   disabled,
   tooltip,
+  loading,
 }) => {
   const handleOnClick = () => {
     if (!disabled) onClick?.();
@@ -45,6 +48,11 @@ export const Button: React.FunctionComponent<Props> = ({
         data-tip={tooltip}
       >
         {children}
+        {loading && (
+          <div className={classes.loading}>
+            <LoadingIcon />
+          </div>
+        )}
       </button>
 
       {tooltip && (
