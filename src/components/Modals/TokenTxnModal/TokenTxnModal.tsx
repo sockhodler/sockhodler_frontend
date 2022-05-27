5a2b0ddc92a2a16637921a7e820cf91251895610
SockHodler <xluxaeternax@protonmail.com>
2022-05-27 08:58:43 -0400

    CSS Modal Styling - Daily Scan Rewards

--- src/components/Modals/TokenTxnModal/TokenTxnModal.tsx
+++ src/components/Modals/TokenTxnModal/TokenTxnModal.tsx
@@ -25,25 +25,38 @@ export const TokenTxnModal: React.FunctionComponent<Props> = ({
       noHeader
       persistent
     >
-      <div className={classes.content}>
-        <p>
-          {data.amount} SOCK token is transferred to{" "}
-          <a href={`https://algoexplorer.io/address/${addr}`}>
-            {formatAddress(addr)}
-          </a>
-        </p>
-        <br />
-        <p>
-          <a
-            href={`https://algoexplorer.io/tx/${data.txId}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Click here
-          </a>{" "}
-          see the tx details.
-        </p>
-        <Button onClick={onClose}>OK</Button>
+      <div className={classes.success}>
+        <div className={classes.success__header}>
+          <h3 className={classes.success__title}>Daily Rewards</h3>
+          <h4 className={classes.success__subtitle}>
+            <span>Claimed</span>
+          </h4>
+        </div>
+
+        <div className={classes.content}>
+          <h3 className={classes.content__number}>{data.amount}</h3>
+          <p>
+            $SOCKS tokens have been transferred to
+            <br />
+            <a href={`https://algoexplorer.io/address/${addr}`}>
+              {formatAddress(addr)}
+            </a>
+          </p>
+          <br />
+          <p>
+            <a
+              href={`https://algoexplorer.io/tx/${data.txId}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Click here
+            </a>{" "}
+            to see the TX details.
+          </p>
+          <Button onClick={onClose} className={classes.button}>
+            OK
+          </Button>
+        </div>
       </div>
     </BaseModal>
   );
